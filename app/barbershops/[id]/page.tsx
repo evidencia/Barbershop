4d522e37ede5ba667dcd182dea314ed072352660
evidencia <evidenciadavid2016@gmail.com>
2024-05-08 01:04:04 +0100

    feat: add BarbershopInfo component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { db } from "@/app/_lib/prisma";
+import BarbershopInfo from "./_components/barbershop-info";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  if (!params.id) {
+    // TODO: redirect to home page
+    return null;
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barbershop) {
+    // TODO: redirect to home page
+    return null;
+  }
+
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+    </div>
+  );
+};
+
+export default BarbershopDetailsPage;
